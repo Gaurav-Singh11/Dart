ce873f4756b0bb9685647a75d2c800bd4e133322
Gaurav <Your>
2023-01-01 11:58:38 +0530

    basics

--- basics /si.dart
+++
@@ -1,7 +0,0 @@
-void main(List<String> args) {
-  double p = 1000;
-  double r = 7;
-  int t = 2;
-  double si = (p * r * t) / 100;
-  print(si);
-}
--- basics /take_input_from_user.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void main(List<String> args) {
-  stdout.write("Please enter your name: ");
-  String name = stdin.readLineSync()!;
-  print("Your name is: $name");
-}
