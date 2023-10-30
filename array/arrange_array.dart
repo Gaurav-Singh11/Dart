c70271396d205f69fa0937d5d764d9ed6772e082
Gaurav <Your>
2023-10-30 21:31:25 +0530

    update

--- array/arrange_array.dart
+++ array/arrange_array.dart
@@ -3,17 +3,14 @@ void main(List<String> args) {
   List<int> arr = List.filled(size, 0, growable: false);
   int i = 0;
   int j = size - 1;
-  int? x;
-  print(x);
-
-  // for (int k = 1; k <= size; k++) {
-  //   if (k % 2 == 0) {
-  //     arr[j] = k;
-  //     j--;
-  //   } else {
-  //     arr[i] = k;
-  //     i++;
-  //   }
-  // }
-  // print(arr);
+  for (int k = 1; k <= size; k++) {
+    if (k % 2 == 0) {
+      arr[j] = k;
+      j--;
+    } else {
+      arr[i] = k;
+      i++;
+    }
+  }
+  print(arr);
 }
---
+++ array/remove_duplicates.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  List<int> num = [1,1,2];
+  List<int> num2 = num.toSet().toList();
+  print(num2);
+}
