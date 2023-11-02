2d3fe7073d3fdf0a3d0866e54d5a1664901e71ed
Gaurav <Your>
2023-11-02 15:30:08 +0530

    parse number with exception handling

--- exception_handling/parse_number.dart
+++ exception_handling/parse_number.dart
@@ -3,7 +3,6 @@ void main() {
   // String value = "12.245";
   // String value = "12";
   String value = "";
-
   if (int.tryParse(value) != null) {
     print(int.parse(value));
   } else if (double.tryParse(value) != null) {
