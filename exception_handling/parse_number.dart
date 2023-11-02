d90c174a8b42638fd44ba0ccb5d80909d8468b1c
Gaurav <Your>
2023-11-02 15:29:19 +0530

    parse number with exception handling

--- exception_handling/parse_number.dart
+++ exception_handling/parse_number.dart
@@ -1,9 +1,13 @@
 void main() {
-  String value = "Infinity";
+  // String value = "Infinity";
+  // String value = "12.245";
+  // String value = "12";
+  String value = "";
+
   if (int.tryParse(value) != null) {
     print(int.parse(value));
   } else if (double.tryParse(value) != null) {
-    return print(double.parse(double.parse(value).toStringAsFixed(2)));
+    print(double.parse(double.parse(value).toStringAsFixed(2)));
   } else {
     print("Exception aa gaya ${value}");
   }
